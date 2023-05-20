73f669864d8eca630a43d82fb3373689fe449095
ivkud <ivkudosh@gmail.com>
2023-05-20 17:47:12 +0300

    fix: fixed some lines

--- api/src/helpers/randoms.ts
+++ api/src/helpers/randoms.ts
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 const random = require("simple-random-number-generator");
-const generator = require('generate-password');
+import { generator } from 'ts-password-generator';
 import { Params } from "./types";
 
 const params: Params = {
@@ -23,10 +23,7 @@ const randomIdForMethodPut: number = random(params);
 const randomIdForMethodPatch: number = random(params);
 const randomIdForMethodDelete: number = random(params);
 
-const randomString: string = generator.generate({
-    length: 18,
-    numbers: true
-});
+const randomString: string = generator({ haveNumbers: true, charsQty: 18, isUppercase: true, haveString: true, haveSymbols: true })
 
 export {
     randomIdForMethodPost,
