fffd20e9b9fd95ddf18a79cb560f67c0e23f016d
ivkud <ivkudosh@gmail.com>
2023-05-22 20:45:09 +0300

    fix: fixed randoms

--- unit/src/helpers/randoms.ts
+++ unit/src/helpers/randoms.ts
@@ -7,23 +7,17 @@ import { generator } from 'ts-password-generator';
 const nameRandom: string = random.first();
 const nameRandomBasic: string = random.first();
 
-
 const emailRandom: string = emails.generateEmail();
 const emailRandomBasic: string = emails.generateEmail();
 
-
 const childAgeRandom: number = randomAge({ type: 'child' });
 const adultAgeRandom: number = randomAge({ type: 'adult' });
-
 const seniorAgeRandomBasic: number = randomAge({ type: 'senior' });
 
-const passwordRandomMax: string = generator({ haveNumbers: true, charsQty: 18, isUppercase: true, haveString: true, haveSymbols: true })
-
-const passwordRandomMaxBasic: string = generator({ haveNumbers: true, charsQty: 16, isUppercase: true, haveString: true, haveSymbols: true })
-
-const passwordRandomMin: string = generator({ haveNumbers: true, charsQty: 8, isUppercase: true, haveString: true, haveSymbols: true })
-
-const invalidPasswordRandom: string = generator({ haveNumbers: true, charsQty: 7, isUppercase: true, haveString: true, haveSymbols: true })
+const passwordRandomMax: string = generator({ haveNumbers: true, charsQty: 18, isUppercase: true, haveString: true, haveSymbols: true });
+const passwordRandomMaxBasic: string = generator({ haveNumbers: true, charsQty: 16, isUppercase: true, haveString: true, haveSymbols: true });
+const passwordRandomMin: string = generator({ haveNumbers: true, charsQty: 8, isUppercase: true, haveString: true, haveSymbols: true });
+const invalidPasswordRandom: string = generator({ haveNumbers: true, charsQty: 7, isUppercase: true, haveString: true, haveSymbols: true });
 
 export {
     nameRandom,
